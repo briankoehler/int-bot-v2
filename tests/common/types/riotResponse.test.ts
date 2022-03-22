155d560fd03e898a622f82e5a96697c74e86593c
briankoehler <briankoehler@ufl.edu>
2022-03-22 18:29:44 -0400

    Add isMatchResponse unit tests

--- tests/common/types/riotResponse.test.ts
+++ tests/common/types/riotResponse.test.ts
@@ -1,7 +1,10 @@
 import {
     ErrorResponse,
     isErrorResponse,
+    isMatchIdsResponse,
+    isMatchResponse,
     isSummonerResponse,
+    MatchResponse,
     SummonerResponse
 } from '../../../src/common/types/riotResponse'
 
@@ -73,5 +76,89 @@ describe('isErrorResponse', () => {
             status: {}
         }
         expect(isErrorResponse(response)).toBe(false)
+    })
+})
+
+describe('isMatchIdsResponse', () => {
+    it('succeeds on array of strings', () => {
+        const response: string[] = ['', '']
+        expect(isMatchIdsResponse(response)).toBe(true)
+    })
+
+    it('fails on non-array', () => {
+        const nonArrays = [undefined, null, '', 0, true, false, {}]
+        nonArrays.forEach(r => expect(isMatchIdsResponse(r)).toBe(false))
+    })
+
+    it('fails when array is not of strings', () => {
+        const response: unknown = ['', 0]
+        expect(isMatchIdsResponse(response)).toBe(false)
+    })
+})
+
+describe('isMatchResponse', () => {
+    it('succeeds on a valid match response', () => {
+        const response: MatchResponse = {
+            metadata: {
+                matchId: '',
+                participants: []
+            },
+            info: {
+                gameCreation: 0,
+                gameDuration: 0,
+                gameEndTimestamp: 0,
+                gameId: 0,
+                gameMode: '',
+                gameStartTimestamp: 0,
+                gameType: '',
+                gameVersion: '',
+                mapId: 0,
+                queueId: 0,
+                participants: []
+            }
+        }
+
+        expect(isMatchResponse(response)).toBe(true)
+    })
+
+    it('fails when not an object', () => {
+        const nonObjects = [undefined, null, '', 0, true, false, []]
+        nonObjects.forEach(r => expect(isMatchResponse(r)).toBe(false))
+    })
+
+    it('fails when a property is null or undefined', () => {
+        const response: MatchResponse = {
+            metadata: {
+                matchId: '',
+                participants: []
+            },
+            info: {
+                gameCreation: 0,
+                gameDuration: 0,
+                gameEndTimestamp: 0,
+                gameId: 0,
+                gameMode: '',
+                gameStartTimestamp: 0,
+                gameType: '',
+                gameVersion: '',
+                mapId: 0,
+                queueId: 0,
+                participants: []
+            }
+        }
+
+        Object.entries(response).forEach(([key]) => {
+            const copy = { ...response }
+            Object.assign(copy, { [key]: null })
+            expect(isMatchResponse(copy)).toBe(false)
+
+            delete (copy as never)[key]
+            expect(isMatchResponse(copy)).toBe(false)
+        })
+    })
+
+    it('fails when metadata missing info', () => {
+        const response: unknown = { metadata: {}, info: {} }
+        expect(isMatchResponse(response)).toBe(false)
     })
 })
